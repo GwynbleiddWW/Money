9387f00287d257989abc07229114d9ac64c2348f
Konstantin Shebalin <championacolironni@gmail.com>
2021-06-16 21:39:11 +0700

    Done**

--- src/main/java/ru/netology/Market.java
+++ src/main/java/ru/netology/Market.java
@@ -11,11 +11,13 @@ public class Market extends Thread {
 
     public void run() {
         long value = ThreadLocalRandom.current().nextLong(150, 20000);
-        long[] money = {value + value * value + value * value + value};
-        long sumOfMoney = 0;
-        for (long moneyValue : money) {
-            sumOfMoney = +moneyValue;
+        long value1 = ThreadLocalRandom.current().nextLong(100, 15000);
+        long value2 = ThreadLocalRandom.current().nextLong(250, 35000);
+        long value3 = ThreadLocalRandom.current().nextLong(50, 270000);
+        long value4 = ThreadLocalRandom.current().nextLong(1200, 71000);
+        long[] money = {value, value1, value2, value3, value4};
+        for (long l : money) {
+            sumOfMarketMoney.addAdder(l);
         }
-        sumOfMarketMoney.addAdder(sumOfMoney);
     }
 }
--- src/main/java/ru/netology/SumOfMarketMoney.java
+++ src/main/java/ru/netology/SumOfMarketMoney.java
@@ -5,11 +5,11 @@ import java.util.concurrent.atomic.LongAdder;
 public class SumOfMarketMoney {
     private final LongAdder adder = new LongAdder();
 
-    public void addAdder(long sumOf){
+    public void addAdder(long sumOf) {
         adder.add(sumOf);
     }
 
-    public long sumAdder(){
+    public long sumAdder() {
         return adder.sum();
     }
 }
