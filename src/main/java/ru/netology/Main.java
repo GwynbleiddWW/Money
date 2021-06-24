eb4c86d3c1ad3394490d4aceaead635e34d3b190
Konstantin Shebalin <championacolironni@gmail.com>
2021-06-25 01:14:15 +0700

    Done***

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -5,11 +5,17 @@ public class Main {
     public static void main(String[] args) throws Exception {
         SumOfMarketMoney sumOfMarketMoney = new SumOfMarketMoney();
 
-        new Market(sumOfMarketMoney).start();
-        new Market(sumOfMarketMoney).start();
-        new Market(sumOfMarketMoney).start();
+        Thread market1 = new Market(sumOfMarketMoney);
+        Thread market2 = new Market(sumOfMarketMoney);
+        Thread market3 = new Market(sumOfMarketMoney);
 
-        Thread.sleep(2000);
+        market1.start();
+        market1.join();
+        market2.start();
+        market2.join();
+        market3.start();
+        market3.join();
+
         System.out.println("Общая выручка магазинов: " + sumOfMarketMoney.sumAdder() + " руб.");
     }
 }
